735bf378a43d4a17a8b0d8d8fafb14c6e3344cb9
smgcp63 <smgcp63@gmail.com>
2025-07-22 12:23:48 -0700

    updated python stats using copilot

--- stats.py
+++ stats.py
@@ -1,15 +1,19 @@
-def cacl(nums)
-total=0
-a=nums[0]
-b=([0]
-for n in nums:
- total=total+n
-  if n>a;
-if n<b;
- b=n
-avg=total/len(nums)
-print{"Sum:",total)
-print("Average:",avg)
-print{Max:",b)
-      list=[3,5,2,8,1,4]
-calc(list)
+def calc(nums):
+    total = 0
+    a = nums[0]
+    b = nums[0]
+    
+    for n in nums:
+        total += n
+        if n > a:
+            a = n
+        if n < b:
+            b = n
+    avg = total / len(nums)
+    print("Sum:", total)
+    print("Average:", avg)
+    print("Max:", a)
+    print("Min:", b)
+
+list_nums = [3, 5, 2, 8, 1, 4]
+calc(list_nums)
