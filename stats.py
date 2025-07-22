5e81b8e0953a545458493dcfcf4dd44198f06a4e
smgcp63 <smgcp63@gmail.com>
2025-07-22 12:19:51 -0700

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,15 @@
+def cacl(nums)
+total=0
+a=nums[0]
+b=([0]
+for n in nums:
+ total=total+n
+  if n>a;
+if n<b;
+ b=n
+avg=total/len(nums)
+print{"Sum:",total)
+print("Average:",avg)
+print{Max:",b)
+      list=[3,5,2,8,1,4]
+calc(list)
